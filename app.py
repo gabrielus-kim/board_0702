1719ac9f75eb6c8d3b8588e6a1dd3ceb9de57242
Gabriel kim <avante789@gmail.com>
2020-07-03 16:55:01 +0900

    add to_csv

--- app.py
+++ app.py
@@ -20,7 +20,14 @@ def index():
 def login():
     owner = 'Hi~ Everyone~'
     if request.method == 'POST':
-        owner = 'welcome login'
+        author = pd.read_csv("C:\\Users\\user\\author.csv", sep=',')        
+        for i in author.index:
+            author_id = author.iloc[i,0]
+            if author_id == request.form['id']:
+                owner = author_id
+            else:
+                owner = author_id
+
     return render_template("login.html",
                             owner = owner)
 
@@ -28,8 +35,17 @@ def login():
 def register():
     owner = "login ID 을 등록해 주세요."
     if request.method == 'POST':
-        owner = request.form['id']
+        author = pd.DataFrame()
+        author['id'] = [request.form['id']]
+        author['password'] = [request.form['pw']]
+        author['profile'] = [request.form['pf']]
+        
+        author.to_csv("C:\\Users\\user\\author.csv",index=False, line_terminator='\n',encoding='utf-8', mode='a', header=False)
+        
+        # author['text'] = author['text'].apply(lambda x: x.encode('ascii', 'ignore').decode('ascii'))
 
+
+        owner = author
     return render_template("register.html",
                             owner = owner)
 
